8765935d9e4028d2a5cbd2ea261a52084a09dcb8
Scubakay <=>
2025-06-03 19:36:49 +0200

    Add 1.20 support
    Add 1.20.2 support
    
    Add 1.20.3-1.20.5 support
    
    Add 1.20.6 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ stonecutter {
     centralScript = "build.gradle.kts"
 
     shared {
-        versions("1.21", "1.21.2")
+        versions("1.20", "1.20.5", "1.21", "1.21.2")
     }
     create(rootProject)
 }
