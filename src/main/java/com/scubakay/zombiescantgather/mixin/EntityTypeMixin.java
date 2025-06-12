579e0b4254c8ab2ceb6193bdd77483a300f1d3b5
Scubakay <=>
2025-06-12 07:13:21 +0200

    #19 Log when an entity carrying forbidden items is loaded #20 Track the entities carrying forbidden items in world nbt

---
+++ src/main/java/com/scubakay/zombiescantgather/mixin/EntityTypeMixin.java
@@ -0,0 +1,45 @@
+package com.scubakay.zombiescantgather.mixin;
+
+import com.scubakay.zombiescantgather.state.EntityTracker;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.PiglinEntity;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Function;
+
+import static com.scubakay.zombiescantgather.ZombiesCantGather.LOGGER;
+import static com.scubakay.zombiescantgather.ZombiesCantGather.MOD_CONFIG;
+
+@Mixin(EntityType.class)
+public class EntityTypeMixin {
+    @Inject(method = "method_17843", at = @At("RETURN"))
+    private static void zombiesCantGather$checkForbiddenItems(NbtCompound nbtCompound, World world, SpawnReason spawnReason, Function<Entity, Entity> function, Entity entity, CallbackInfoReturnable<Entity> cir) {
+        if (entity instanceof ZombieEntity zombie) {
+            ItemStack item = zombie.getHandItems().iterator().next();
+            if (!world.isClient() && MOD_CONFIG.zombiesCantGather.get().contains(item.getItem().toString())) {
+                EntityTracker state = EntityTracker.getWorldState((ServerWorld) world);
+                state.trackedEntities.put(entity.getUuid(), nbtCompound);
+                state.markDirty();
+                LOGGER.info("Found {} with forbidden item: {} at position {}", entity.getDisplayName(), item.getItem(), zombie.getPos());
+            }
+        } else if (entity instanceof PiglinEntity piglin) {
+            ItemStack item = piglin.getHandItems().iterator().next();
+            if (!world.isClient() && MOD_CONFIG.zombiesCantGather.get().contains(item.getItem().toString())) {
+                EntityTracker state = EntityTracker.getWorldState((ServerWorld) world);
+                state.trackedEntities.put(entity.getUuid(), nbtCompound);
+                state.markDirty();
+                LOGGER.info("Found {} with forbidden item: {} at position {}", entity.getDisplayName(), item.getItem(), piglin.getPos());
+            }
+        }
+    }
+}
---
+++ src/main/java/com/scubakay/zombiescantgather/state/EntityTracker.java
@@ -0,0 +1,49 @@
+package com.scubakay.zombiescantgather.state;
+
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.PersistentState;
+import net.minecraft.world.PersistentStateManager;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import static com.scubakay.zombiescantgather.ZombiesCantGather.MOD_ID;
+
+public class EntityTracker extends PersistentState {
+    public static final String TRACKED_ENTITIES = "TrackedEntities";
+
+    public HashMap<UUID, NbtCompound> trackedEntities = new HashMap<>();
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
+        NbtCompound entitiesNbt = new NbtCompound();
+        trackedEntities.forEach((uuid, entity) -> entitiesNbt.put(uuid.toString(), entity));
+        nbt.put(TRACKED_ENTITIES, entitiesNbt);
+        return nbt;
+    }
+
+    public static EntityTracker createFromNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
+        EntityTracker state = new EntityTracker();
+        NbtCompound entitiesNbt = tag.getCompound(TRACKED_ENTITIES);
+        entitiesNbt.getKeys().forEach(key -> {
+            NbtCompound entityNbt = entitiesNbt.getCompound(key);
+            state.trackedEntities.put(UUID.fromString(key), entityNbt);
+        });
+        return state;
+    }
+
+    private static final Type<EntityTracker> type = new Type<>(
+            EntityTracker::new,
+            EntityTracker::createFromNbt,
+            null
+    );
+
+    public static EntityTracker getWorldState(ServerWorld world) {
+        PersistentStateManager manager = world.getPersistentStateManager();
+        EntityTracker state = manager.getOrCreate(type, MOD_ID);
+        state.markDirty();
+        return state;
+    }
+}
