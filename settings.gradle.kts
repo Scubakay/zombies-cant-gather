341a6d8483fd6779817d07d9b733c8f5ea1981e0
Scubakay <github-action@actions-template-sync.noreply.github.com>
2025-07-11 10:27:56 +0000

    chore(template): merge template changes :up:
    Signed-off-by: Scubakay <github-action@actions-template-sync.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,10 +17,46 @@ stonecutter {
 
     shared {
         vers("dev", "1.21.7")
-        versions("1.21.6", "1.21.5", "1.21.2", "1.21")
+        versions("1.21.6")
         vcsVersion = "dev"
     }
     create(rootProject)
 }
 
-rootProject.name = "Zombies Can't Gather"
+//region Git precommit hook
+gradle.beforeProject {
+    val gitDir = rootDir.resolve(".git")
+    if (gitDir.exists() && gitDir.isDirectory) {
+        val hooksDir = gitDir.resolve("hooks")
+        val preCommitHook = hooksDir.resolve("pre-commit")
+
+        if (!preCommitHook.exists()) {
+            hooksDir.mkdirs()
+            preCommitHook.writeText(
+                """
+                #!/bin/bash
+                
+                vcs_version=$(ggrep -oP 'vcsVersion\s*=\s*"\K[^"]+' settings.gradle.kts)
+                active_version=$(ggrep -oP 'stonecutter\s+active\s+"\K[^"]+' stonecutter.gradle.kts)
+                
+                echo "Detected vcsVersion: ${'$'}vcs_version"
+                echo "Detected active version: ${'$'}active_version"
+                
+                if [ "${'$'}vcs_version" != "${'$'}active_version" ]; then
+                  echo "Please run './gradlew \"Reset active project\"' to set the stonecutter branch to the version control version."
+                  exit 1
+                else
+                  echo "Versions match. No action needed."
+                fi
+                """.trimIndent()
+            )
+            preCommitHook.setExecutable(true)
+            println("Git pre-commit hook installed.")
+        }
+    } else {
+        println("Not a Git repository. Skipping hook installation.")
+    }
+}
+//endregion
+
+rootProject.name = "Stonecutter Template"
